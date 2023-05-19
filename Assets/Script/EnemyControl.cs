6cb527e6bebd17b2a6f737b88417b8f74923d05d
hoshi syuuto <syu111.yu528@gmail.com>
2023-05-19 12:32:00 +0900

    三回目

---
+++ Assets/Script/EnemyControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControl : MonoBehaviour
+{
+        //public GameObject player; // 玉のオブジェクト
+
+        //public GameObject Enemy;
+
+        //private Vector3 offset; // 玉からカメラまでの距離
+
+
+        [SerializeField] private GameObject player;
+        private Rigidbody rb;
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //毎フレーム力を加える
+            rb.AddForce(player.transform.position - transform.position);
+            //何もついてないtransform.positionは、アタッチされている
+            //物体の座標になる
+            
+        }
+}
